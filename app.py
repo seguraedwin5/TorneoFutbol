c99c5f4c4d727b04f713c81cb24f365825a073cb
Diego Lizarazo <imdlizarazo@gmail.com>
2024-03-20 13:29:35 -0500

    Edit index.htlm
    Se agrega visualización básica de los equipos participantes

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-teams = ["Equipo 1",
-         "Equipo 2",
-         "Equipo 3",
-         "Equipo 4"]
+# Lista de equipos
+teams = ["Millonarios",
+         "Nacional",
+         "America",
+         "Junio"]
 
 @app.route('/')
 def index():
-     return render_template('index.html')
+    return render_template('index.html', teams=teams)
 
 if __name__ == '__main__':
     app.run(debug=True)
