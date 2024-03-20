a68c05f5ea7b645fa2df13d7c513b4e65b19cd47
Diego Lizarazo <imdlizarazo@gmail.com>
2024-03-20 14:54:49 -0500

    Edit app.py index.html
    Se implementa el funcionamiento básico del proyecto

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, jsonify, request, render_template_string
 
 app = Flask(__name__)
 
@@ -8,9 +8,40 @@ teams = ["Millonarios",
          "America",
          "Junio"]
 
+# Diccionario de Victorias por Equipos
+victories = {team: 0 for team in teams}
+
+# Index
 @app.route('/')
 def index():
-    return render_template('index.html', teams=teams)
+    rounds = [
+        (teams[0], teams[1]),
+        (teams[2], teams[3]),
+        (teams[0], teams[2]),
+        (teams[1], teams[3])
+    ]
+    
+    return render_template_string(
+        open('templates/index.html').read(),
+        equipos=teams,
+        partidos=rounds
+    )
+
+# Ruta: Obtener Ganador
+@app.route('/get_winner', methods=['POST'])
+def get_winner():
+    
+    results = request.json['resultados']
+    
+    for result in results:
+        victories[result] += 1
+    
+    winner = max(victories, key=victories.get)
+    
+    print(winner)
+    
+    return jsonify({'winner': winner})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
